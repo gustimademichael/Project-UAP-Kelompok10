0d83bef860f84045a4f0f13e7ae2bf64e74bf0bc
Nadia Putri Azhara <nadiaajja673@gmail.com>
2025-12-09 20:11:36 +0700

    ganti isi menu about

--- about.cpp
+++ about.cpp
@@ -14,13 +14,16 @@ void about() {
     keypad(w, TRUE);
 
     mvwprintw(w, 2, 2, "ABOUT GAME");
-    mvwprintw(w, 4, 2, "Game ini diciptakan oleh Kelompok 7 yang");
-    mvwprintw(w, 5, 2, "yang beranggotakan : ");
-    mvwprintw(w, 7, 2, "   1. Gusti Made Michael Mahatma");
-    mvwprintw(w, 8, 2, "   2. Nadia Putri Azzahra");
-    mvwprintw(w, 9, 2, "   3. Siti Zulaika");
-    mvwprintw(w, 11, 2, "game ini terinspiasi dari minigame yang ada");
-    mvwprintw(w, 12, 2, "di game pou yang diusulkan dari nadia");
+    mvwprintw(w, 4, 2, "KECEMPLUNG adalah sebuah game kasual yang menantang pemain untuk menangkap");
+    mvwprintw(w, 5, 2, "objek yang jatuh dari atas layar. Pemain mengendalikan sebuah keranjang untuk");
+    mvwprintw(w, 7, 2, "menangkap objek tersebut. Tujuan utama game ini adalah untuk menangkap sebanyak-banyaknya");
+    mvwprintw(w, 8, 2, "objek yang jatuh dari atas kedalam keranjang agar mendapatkan skor tertinggi.");
+    mvwprintw(w, 9, 2, "Apabila objek yang jatuh dari atas tidak tertangkap sebanyak 3 kali maka permainan selesai.");
+    mvwprintw(w, 10, 2, "Game ini dibuat oleh kelompok 10, yang beranggotakan: ");
+    mvwprintw(w, 11, 2, "   1. Gusti Made Michael Mahatma");
+    mvwprintw(w, 12, 2, "   2. Nadia Putri Azahra");
+    mvwprintw(w, 13, 2, "   3. Siti Zulaika");
+    mvwprintw(w, 14, 2, "game ini terinspiasi dari minigame yang ada di game pou");
 
     mvwprintw(w, 18, 2, "Press any key to go back...");
     wrefresh(w);
